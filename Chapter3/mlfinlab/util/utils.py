4c78494525143a7d588d61d888fa207076245f22
Jackal08 <jacques@quantsportal.com>
2019-03-16 21:21:44 +0200

    Fixing pyfolio metrics

--- Chapter3/mlfinlab/util/utils.py
+++ Chapter3/mlfinlab/util/utils.py
@@ -33,6 +33,20 @@ def plot_multi(data, cols=None, spacing=.1, **kwargs):
     return ax
 
 
+def get_daily_returns(intraday_returns):
+    # Todo: Add doc string
+    
+    cum_rets = ((intraday_returns + 1).cumprod())
+
+    # Downsample to daily
+    daily_rets = cum_rets.resample('D').last()
+
+    # Forward fill, Percent Change, Drop NaN
+    daily_rets = daily_rets.ffill().pct_change().dropna()
+    
+    return daily_rets
+
+
 def cprint(df):
     if not isinstance(df, (pd.DataFrame, dd.DataFrame)):
         try:
