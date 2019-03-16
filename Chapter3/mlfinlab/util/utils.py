fdd6c0faa6f322f23b6fb43882cea3f80fa34b9a
Asingh <singh.ashu@gmail.com>
2019-03-16 19:37:10 -0400

    Changed resampling from daily to business day

--- Chapter3/mlfinlab/util/utils.py
+++ Chapter3/mlfinlab/util/utils.py
@@ -6,7 +6,7 @@ def get_daily_returns(intraday_returns):
     cum_rets = ((intraday_returns + 1).cumprod())
 
     # Downsample to daily
-    daily_rets = cum_rets.resample('D').last()
+    daily_rets = cum_rets.resample('B').last()
 
     # Forward fill, Percent Change, Drop NaN
     daily_rets = daily_rets.ffill().pct_change().dropna()
