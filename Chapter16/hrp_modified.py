ffb3fe57a63e4b937ed28d6eb942cfbaf2b5fdd4
Aditya Vyas <adityavyas17@gmail.com>
2019-08-31 17:40:06 -0400

    Update notebook

--- Chapter16/hrp_modified.py
+++ Chapter16/hrp_modified.py
@@ -2,153 +2,39 @@ import numpy as np
 import pandas as pd
 from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.spatial.distance import squareform
+from sklearn.covariance import OAS
 import matplotlib.pyplot as plt
+from mlfinlab.portfolio_optimization.hrp import HierarchicalRiskParity
 
 
-class HierarchicalRiskParityModified:
-
-    def __init__(self):
-        return
-
-    def _tree_clustering(self, correlation, method='single'):
-        '''
-        Perform the traditional heirarchical tree clustering
-
-        :param correlation: (np.array) correlation matrix of the assets
-        :param method: (str) the type of clustering to be done
-        :return: distance matrix and clusters
-        '''
-
-        distances = np.sqrt((1 - correlation) / 2)
-        clusters = linkage(squareform(distances.values), method=method)
-        return distances, clusters
-
-    def _quasi_diagnalization(self, N, curr_index):
-        '''
-        Rearrange the assets to reorder them according to hierarchical tree clustering order.
-
-        :param N: (int) index of element in the cluster list
-        :param curr_index: (int) current index
-        :return: (list) the assets rearranged according to hierarchical clustering
-        '''
-
-        if curr_index < N:
-            return [curr_index]
-
-        left = int(self.clusters[curr_index - N, 0])
-        right = int(self.clusters[curr_index - N, 1])
-
-        return (self._quasi_diagnalization(N, left) + self._quasi_diagnalization(N, right))
-
-    def _get_seriated_matrix(self, assets, distances, correlations):
-        '''
-        Based on the quasi-diagnalization, reorder the original distance matrix, so that assets within
-        the same cluster are grouped together.
-
-        :param assets:
-        :param distances:
-        :param correlations:
-        :return: (np.array) re-arranged distance matrix based on tree clusters
-        '''
-
-        ordering = assets[self.ordered_indices]
-        seriated_distances = distances.loc[ordering, ordering]
-        seriated_correlations = correlations.loc[ordering, ordering]
-        return seriated_distances, seriated_correlations
-
-    def _recursive_bisection(self, covariances, assets):
-        '''
-        Recursively assign weights to the clusters - ultimately assigning weights to the inidividual assets
-
-        :param covariances: (np.array) the covariance matrix
-        :param assets:
-        '''
-
-        self.weights = pd.Series(1, index=self.ordered_indices)
-        clustered_alphas = [self.ordered_indices]
-
-        while len(clustered_alphas) > 0:
-            clustered_alphas = [cluster[start:end]
-                                for cluster in clustered_alphas
-                                for start, end in ((0, len(cluster) // 2), (len(cluster) // 2, len(cluster)))
-                                if len(cluster) > 1]
-
-            for subcluster in range(0, len(clustered_alphas), 2):
-                left_cluster = clustered_alphas[subcluster]
-                right_cluster = clustered_alphas[subcluster + 1]
-
-                # Get left cluster variance
-                left_subcovar = covariances.iloc[left_cluster, left_cluster]
-                inv_diag = 1 / np.diag(left_subcovar.values)
-                parity_w = inv_diag * (1 / np.sum(inv_diag))
-                left_cluster_var = np.dot(parity_w, np.dot(left_subcovar, parity_w))
-
-                # Get right cluster variance
-                right_subcovar = covariances.iloc[right_cluster, right_cluster]
-                inv_diag = 1 / np.diag(right_subcovar.values)
-                parity_w = inv_diag * (1 / np.sum(inv_diag))
-                right_cluster_var = np.dot(parity_w, np.dot(right_subcovar, parity_w))
-
-                # Calculate allocation factor and weights
-                alloc_factor = 1 - left_cluster_var / (left_cluster_var + right_cluster_var)
-                self.weights[left_cluster] *= alloc_factor
-                self.weights[right_cluster] *= 1 - alloc_factor
-
-        # Assign actual asset values to weight index
-        self.weights.index = assets[self.ordered_indices]
-        self.weights = pd.DataFrame(self.weights)
-        self.weights = self.weights.T
-
-    def plot_clusters(self, height=10, width=10):
-        '''
-        Plot a dendrogram of the hierarchical clusters
-
-        :param height: (int) height of the plot
-        :param width: (int) width of the plot
-        '''
-
-        plt.figure(figsize=(width, height))
-        dendrogram(self.clusters)
-        plt.show()
-
-    def _calculate_returns(self, asset_prices, resample_returns_by):
-        '''
-
-        :param asset_prices: (pd.Dataframe/np.array) the matrix of historical asset prices (daily close)
-        :param resample_returns_by: (str) specifies how to resample the returns - weekly, daily, monthly etc.. Defaults to
-                                  'B' meaning daily business days which is equivalent to no resampling
-        :return: (pd.Dataframe) stock returns
-        '''
-        asset_returns = asset_prices.pct_change()
-        asset_returns = asset_returns.dropna(how='all')
-        asset_returns = asset_returns.resample(resample_returns_by).mean()
-        return asset_returns
-
-    def allocate(self, asset_prices, covariance, resample_returns_by='B'):
-        """
-        Calculate asset allocations using HRP algorithm
-
-        :param asset_prices: (pd.Dataframe/np.array) the matrix of historical asset prices (daily close)
-                                                     indexed by date
-        :param resample_returns_by: (str) specifies how to resample the returns - weekly, daily, monthly etc.. Defaults to
-                                          'B' meaning daily business days which is equivalent to no resampling
-        """
-
-        if type(asset_prices) != pd.DataFrame:
-            asset_prices = pd.DataFrame(asset_prices)
+class HierarchicalRiskParityModified(HierarchicalRiskParity):
+    
+    def allocate(self, asset_prices, covariance, resample_by='B', use_shrinkage=False):
+        if not isinstance(asset_prices, pd.DataFrame):
+            raise ValueError("Asset prices matrix must be a dataframe")
+        if not isinstance(asset_prices.index, pd.DatetimeIndex):
+            raise ValueError("Asset prices dataframe must be indexed by date.")
 
         # Calculate the returns
-        asset_returns = self._calculate_returns(asset_prices, resample_returns_by=resample_returns_by)
+        asset_returns = self._calculate_returns(asset_prices, resample_by=resample_by)
 
-        N = asset_returns.shape[1]
+        num_assets = asset_returns.shape[1]
         assets = asset_returns.columns
-        cov, corr = pd.DataFrame(covariance), asset_returns.corr()
+
+        
+        ### Using the modified covariance matrix ###
+        cov = pd.DataFrame(covariance, columns=assets, index=assets)
+        
+        
+        if use_shrinkage:
+            cov = self._shrink_covariance(covariance=cov)
+        corr = self._cov2corr(covariance=cov)
 
         # Step-1: Tree Clustering
         distances, self.clusters = self._tree_clustering(correlation=corr)
 
         # Step-2: Quasi Diagnalization
-        self.ordered_indices = self._quasi_diagnalization(N, 2 * N - 2)
+        self.ordered_indices = self._quasi_diagnalization(num_assets, 2 * num_assets - 2)
         self.seriated_distances, self.seriated_correlations = self._get_seriated_matrix(assets=assets,
                                                                                         distances=distances,
                                                                                         correlations=corr)
--- Chapter16/mvo_modified.py
+++ Chapter16/mvo_modified.py
@@ -1,57 +1,28 @@
 import numpy as np
 import pandas as pd
+from mlfinlab.portfolio_optimization.mean_variance import MeanVarianceOptimisation
 
 
-class MeanVarianceOptimisationModified:
-
-    def __init__(self):
-        return
-
-    def allocate(self, asset_prices, covariance, solution='inverse_variance', resample_returns_by='B'):
-        '''
-
-        :param asset_prices: (pd.Dataframe/np.array) the matrix of historical asset prices (daily close)
-        :param solution: (str) the type of solution/algorithm to use to calculate the weights
-        :param resample_returns_by: (str) specifies how to resample the returns - weekly, daily, monthly etc.. Defaults to
-                                  'B' meaning daily business days which is equivalent to no resampling
-        '''
-
+class MeanVarianceOptimisationModified(MeanVarianceOptimisation):
+    
+    def allocate(self, asset_prices, covariance, solution='inverse_variance', resample_by='B'):
         if not isinstance(asset_prices, pd.DataFrame):
-            asset_prices = pd.DataFrame(asset_prices)
+            raise ValueError("Asset prices matrix must be a dataframe")
+        if not isinstance(asset_prices.index, pd.DatetimeIndex):
+            raise ValueError("Asset prices dataframe must be indexed by date.")
 
         # Calculate returns
-        asset_returns = self._calculate_returns(asset_prices, resample_returns_by=resample_returns_by)
+        asset_returns = self._calculate_returns(asset_prices, resample_by=resample_by)
         assets = asset_prices.columns
 
-        self.weights = []
         if solution == 'inverse_variance':
-            self.weights = self._inverse_variance(asset_returns=asset_returns, covariance=covariance)
+            
+            ### Using the modified covariance matrix ###
+            cov = pd.DataFrame(covariance, columns=assets, index=assets)
+            
+            self.weights = self._inverse_variance(covariance=cov)
+        else:
+            raise ValueError("Unknown solution string specified. Supported solutions - inverse_variance.")
         self.weights = pd.DataFrame(self.weights)
         self.weights.index = assets
         self.weights = self.weights.T
-
-    def _calculate_returns(self, asset_prices, resample_returns_by):
-        '''
-
-        :param asset_prices: (pd.Dataframe/np.array) the matrix of historical asset prices (daily close)
-        :param resample_returns_by: (str) specifies how to resample the returns - weekly, daily, monthly etc.. Defaults to
-                                  'B' meaning daily business days which is equivalent to no resampling
-        :return: (pd.Dataframe) stock returns
-        '''
-
-        asset_returns = asset_prices.pct_change()
-        asset_returns = asset_returns.dropna(how='all')
-        asset_returns = asset_returns.resample(resample_returns_by).mean()
-        return asset_returns
-
-    def _inverse_variance(self, asset_returns, covariance):
-        '''
-
-        :param asset_prices: (pd.Dataframe/np.array) the matrix of historical asset prices (daily close)
-        :return: (np.array) array of portfolio weights
-        '''
-
-        cov = pd.DataFrame(covariance)
-        ivp = 1. / np.diag(cov)
-        ivp /= ivp.sum()
-        return ivp
